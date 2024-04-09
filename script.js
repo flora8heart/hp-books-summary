0911760f6954c3c49e28674fc459ff242f5dd481
flora8heart <9201125+flora8heart@users.noreply.github.com>
2024-04-09 06:50:23 +0100

    add loop to change background color.
    If the background color is pastel pink, set background color to pastel
    purple. Else, set the background color back to pastel pink.

--- script.js
+++ script.js
@@ -3,7 +3,17 @@
 // Declare and initialise color button variable
 const colorTogglebtn = document.querySelector("#toggle-color");
 
+// Set a boolean value to an initial value of true to track background color change
+isPink = true;
+
 // Set button to listen for click and create anonymous function to change background color
 colorTogglebtn.addEventListener('click', function(event){
-  document.body.style.backgroundColor = '#DBCDF0';
+  if (isPink == true){
+    document.body.style.backgroundColor = '#DBCDF0';
+    isPink = false;
+  }
+  else {
+    document.body.style.backgroundColor = '#fff1f5';
+    isPink = true;
+  }
 });
